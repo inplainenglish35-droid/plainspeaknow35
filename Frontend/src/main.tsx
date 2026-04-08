1efc5ad6dfac4ea4395e64deed9da065175e6667
Ronicka_Goodwin <inplainenglish35@gmail.com>
2026-04-08 14:36:17 -0600

    deploy test - confirmed correct repo

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,16 +1,1 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import AppLayout from "./components/AppLayout";
-import { AuthProvider } from "./components/plainspeak/contexts/AuthContext";
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <AppLayout />
-      </AuthProvider>
-    </BrowserRouter>
-  </StrictMode>
-);
+document.body.innerHTML = "<h1 style='color:red'>NEW DEPLOY WORKING</h1>";

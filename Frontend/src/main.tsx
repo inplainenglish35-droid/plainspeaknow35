7a99f7b15695dc67ed0aa5420e7f1db5e48e464e
Ronicka_Goodwin <inplainenglish35@gmail.com>
2026-04-08 15:20:25 -0600

    add BrowserRouter wrapper

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -4,11 +4,14 @@ import App from "./App";
 import "./index.css";
 
 import { AuthProvider } from "./components/plainspeak/contexts/AuthContext";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
